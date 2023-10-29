40b41a3b61322543915a4f7025b6cf5e991bcc12
Felipe <felipe.pita@gmail.com>
2023-10-29 14:26:59 +0100

    Improve service league

--- src/league/services/league.service.ts
+++ src/league/services/league.service.ts
@@ -1,5 +1,5 @@
 import {
-  BadRequestException,
+  BadRequestException, ConflictException,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -11,6 +11,8 @@ import { League } from '../entities/league.entity';
 import { In, Repository } from 'typeorm';
 import { isUUID } from 'class-validator';
 import { UserLeague } from '../entities/leagues_users.entity';
+import { LeagueResponseDto } from '../dto/response-league.dto';
+import { LeagueUsersResponseDto } from '../dto/response-league_users.dto';
 
 @Injectable()
 export class LeagueService {
@@ -24,42 +26,75 @@ export class LeagueService {
   ) {}
 
   async create(createLeagueDto: CreateLeagueDto) {
-    try {
-      const { participants, ...leagueDetails } = createLeagueDto;
-      const league = this.leagueRepository.create({
-        ...leagueDetails,
-        participants: [],
-      });
-      if (participants && participants.length > 0) {
-        const users = await this.userRepository.find({
-          where: { id: In(participants) },
-        });
-
-        if (users.length !== participants.length) {
-          throw new BadRequestException('One or more participants not found');
-        }
-
-        league.participants = users;
-      }
-
-      await this.leagueRepository.save(league);
-      return league;
-    } catch (error) {
-      console.log('error league', error);
+    const { participants, ...leagueDetails } = createLeagueDto;
+    let users: User[] = [];
+    const existingLeague = await this.leagueRepository.findOne({
+      where: { name: leagueDetails.name }
+    });
+    if (existingLeague) {
+      throw new ConflictException('Ya existe una liga con este nombre.');
     }
+    if (participants && participants.length > 0) {
+      users = await this.userRepository.find({
+        where: { id: In(participants) },
+        select:['id', 'email', 'name', 'surname', 'isActive', 'createdAt', 'lastLogin', 'elo', 'roles'],
+      });
+
+      if (users.length !== participants.length) {
+        throw new BadRequestException('One or more participants not found');
+      }
+    }
+
+    const league = this.leagueRepository.create({
+      ...leagueDetails,
+      participants: users.length > 0 ? users : [],
+    });
+
+    await this.leagueRepository.save(league);
   }
 
-  findAll() {
-    return this.leagueRepository.find({
+  async findAll(): Promise<LeagueResponseDto[]> {
+    const leagues = await this.leagueRepository.find({
       relations: { participants: true },
     });
+
+    return Promise.all(leagues.map(league => this.mapLeagueToDTO(league)));
   }
+
+  private async mapLeagueToDTO(league: League): Promise<LeagueResponseDto> {
+    const participants = await this.getAndMapParticipants(league);
+    return {
+      id: league.id,
+      name: league.name,
+      isActive: league.isActive,
+      totalMatches: league.totalMatches,
+      matchesWon: league.matchesWon,
+      matchesTied: league.matchesTied,
+      matchesLost: league.matchesLost,
+      points: league.points,
+      createdAt: league.createdAt,
+      participants: participants
+    };
+  }
+
+  private async getAndMapParticipants(league: League): Promise<LeagueUsersResponseDto[]> {
+    const userLeagues = await this.getUserLeaguesForLeague(league);
+    return this.mapUserLeague(userLeagues);
+  }
+
+  private async getUserLeaguesForLeague(league: League): Promise<UserLeague[]> {
+    return this.userLeagueRepository.find({
+      where: { leaguesId: league.id },
+      relations: { user: true },
+    });
+  }
+
 
   /**
    * Method that returns a user searched for by an id or by a name
    * @param param id or name
    */
-  async findOne(param: string) {
+  async findOne(param: string): Promise<LeagueResponseDto> {
     let league: League;
     if (isUUID(param)) {
       league = await this.leagueRepository.findOne({
@@ -72,33 +107,18 @@ export class LeagueService {
 
     if (!league) throw new NotFoundException(`League with ${param} not found`);
 
-    const userLeague = await this.userLeagueRepository.find({
+    const userLeague: UserLeague[] = await this.userLeagueRepository.find({
       where: { leaguesId: league.id },
       relations: { user: true },
     });
-    // console.log(userLeague);
-    const testt = {
-      ...league,
-      participants: this.mapAll(userLeague),
 
-      // participants: userLeague.map((userLeague) => ({
-      //   // Mapeo de los campos necesarios del userLeague
-      //   totalMatches: userLeague.totalMatches,
-      //   matchesWon: userLeague.matchesWon,
-      //   user: {
-      //     email: userLeague.users.email,
-      //     name: userLeague.user.name,
-      //     surname: userLeague.user.surname,
-      //     isActive: userLeague.user.isActive,
-      //     // Otros campos de usuario que necesitas
-      //   },
-      // })),
+    return {
+      ...league,
+      participants: this.mapUserLeague(userLeague),
     };
-    console.log('testss', testt);
-    return testt;
   }
 
-  mapAll(userLeague: UserLeague[]) {
+  mapUserLeague(userLeague: UserLeague[]): LeagueUsersResponseDto[] {
     return userLeague.map((userLeague) => ({
       usersId: userLeague.usersId,
       totalMatches: userLeague.totalMatches,
@@ -113,24 +133,19 @@ export class LeagueService {
     }));
   }
 
-  async findLeagues(param: string) {
+  async findLeagues(param: string): Promise<LeagueResponseDto[]> {
     let leagues: League[];
+    leagues = await this.findByName(param);
 
-    if (isUUID(param)) {
-      leagues = await this.leagueRepository.find({
-        where: { id: param },
-        relations: { participants: true },
-      });
-    } else {
-      leagues = await this.findByName(param);
+    if (leagues.length === 0) {
+      throw new NotFoundException(`No leagues found matching ${param}`);
     }
 
-    if (leagues.length === 0)
-      throw new NotFoundException(`No leagues found matching ${param}`);
-    return leagues;
+    return Promise.all(leagues.map(league => this.mapLeagueToDTO(league)));
+
   }
 
-  async update(id: string, updateLeagueDto: UpdateLeagueDto) {
+  async update(id: string, updateLeagueDto: UpdateLeagueDto): Promise<LeagueResponseDto> {
     const { participants, ...leagueDetails } = updateLeagueDto;
     const league = await this.findOne(id);
 
@@ -156,13 +171,20 @@ export class LeagueService {
 
     try {
       await this.leagueRepository.save(update);
-      return update;
+      const userLeague: UserLeague[] = await this.userLeagueRepository.find({
+        where: { leaguesId: league.id },
+        relations: { user: true },
+      });
+      return {
+        ...update,
+        participants: this.mapUserLeague(userLeague),
+      };
     } catch (err) {
       console.log('Error updating league', err);
     }
   }
 
-  async remove(id: string) {
+  async remove(id: string): Promise<League> {
     const league = await this.leagueRepository.preload({
       id: id,
       isActive: false,
