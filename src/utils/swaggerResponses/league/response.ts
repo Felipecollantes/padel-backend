003aa77d27a182fc78154abf0958dae64e29f57b
Felipe <felipe.pita@gmail.com>
2023-10-29 16:22:58 +0100

    Add api responses

--- src/utils/swaggerResponses/league/response.ts
+++ src/utils/swaggerResponses/league/response.ts
@@ -60,15 +60,15 @@ export const leaguesResponse = {
       points: 23,
       participants: [
         {
-          "id": "506cdf67-010c-4c05-9c95-1c834f5ff484",
-          "email": "nameTest@gmail.com",
-          "name": "Name",
-          "surname": "Testing",
-          "isActive": true,
-          "createdAt": "2023-10-15T10:46:22.791Z",
-          "lastLogin": null,
-          "elo": 1200,
-          "roles": [
+          id: "506cdf67-010c-4c05-9c95-1c834f5ff484",
+          email: "nameTest@gmail.com",
+          name: "Name",
+          surname: "Testing",
+          isActive: true,
+          createdAt: "2023-10-15T10:46:22.791Z",
+          lastLogin: null,
+          elo: 1200,
+          roles: [
             "user"
           ]
         },
@@ -78,6 +78,25 @@ export const leaguesResponse = {
     }
   ]
 };
+
+export const leagueDeleteResponse = {
+  status: 201,
+  description: 'The league has been successfully created.',
+  type: LeagueResponseDto,
+  example: {
+    name: "Champions League",
+    isActive: false,
+    totalMatches: 10,
+    matchesWon: 7,
+    matchesTied: 2,
+    matchesLost: 1,
+    points: 23,
+    participants: [],
+    id: "fb00f78d-a930-45b9-b810-9f12f7ea6596",
+    createdAt: "2023-10-28T09:08:06.757Z"
+  }
+};
+
 
 export const leagueBadRequestResponse = {
   status: 400,
@@ -90,7 +109,7 @@ export const leagueNotFoundResponse = {
 };
 
 
-export const createLeagueApiResponses = [
+export const leagueApiResponses = [
   leagueCreatedResponse,
   leagueBadRequestResponse,
   leagueNotFoundResponse,
@@ -98,6 +117,12 @@ export const createLeagueApiResponses = [
 
 export const leaguesApiResponses = [
   leaguesResponse,
+  leagueBadRequestResponse,
+  leagueNotFoundResponse,
+];
+
+export const leagueDeleteApiResponses = [
+  leagueDeleteResponse,
   leagueBadRequestResponse,
   leagueNotFoundResponse,
 ];
